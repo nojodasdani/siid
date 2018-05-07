4e235097f82b64e50210ffb65178d598c3a127b3
Daniel Enríquez Barrera <danieel_eb_94@hotmail.com>
2018-05-07 00:24:06 -0500

    avance graficas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,33 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
+    <?php
+    use App\Aviso;
+    ?>
     <div class="container">
-        <?php
-        use App\Aviso;
-        $avisos = Aviso::all()->where('visible', '=', 1);
-        $i = 1;
-        foreach ($avisos as $aviso) {
-            if ($i == 5) $i = 1;
-            switch ($i) {
-                case 1:
-                    $clase = "alert-info";
-                    break;
-                case 2:
-                    $clase = "alert-success";
-                    break;
-                case 3:
-                    $clase = "alert-danger";
-                    break;
-                case 4:
-                    $clase = "alert-warning";
-                    break;
-            }
-            echo "<div class='alert $clase alert-dismissible'>
+        @if (Auth::user()->hasRole("Administrador"))
+            <div class="row">
+                <div class="col-md-6">
+                    Gráfica 1
+                </div>
+                <div class="col-md-6">
+                    Gráfica 2
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    Gráfica 3
+                </div>
+                <div class="col-md-6">
+                    Gráfica 4
+                </div>
+            </div>
+        @else
+            <?php
+            $avisos = Aviso::all()->where('visible', '=', 1);
+            $i = 1;
+            foreach ($avisos as $aviso) {
+                if ($i == 5) $i = 1;
+                switch ($i) {
+                    case 1:
+                        $clase = "alert-info";
+                        break;
+                    case 2:
+                        $clase = "alert-success";
+                        break;
+                    case 3:
+                        $clase = "alert-danger";
+                        break;
+                    case 4:
+                        $clase = "alert-warning";
+                        break;
+                }
+                echo "<div class='alert $clase alert-dismissible'>
                     <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
                     $aviso->texto
                   </div>";
-            $i++;
-        }
-        ?>
+                $i++;
+            }
+            ?>
+        @endif
     </div>
 @endsection
